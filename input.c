bc353b8aa5e9051c4357733793c856fc2584c747
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-12-23 19:54:04 +0530

    Add files via upload

---
+++ input.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    // int age;
+    // printf("enter your age here \n");
+    // scanf("%d", &age);
+    // printf("you have entered your age as %d \n", age);   
+    float a;
+    // int b;
+    printf("enter your first number \n");
+    scanf("%f", &a);
+    // printf("enter your second number \n");
+    // scanf("%d", &b);
+    printf("The sum of two numbers is %f" , 3.14*a*a);
+    return 0;
+}
