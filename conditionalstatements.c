adf90ef8b7604fbda2a4795f345364e6e758c7b8
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-12-23 19:49:04 +0530

    Add files via upload

---
+++ conditionalstatements.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+int main(){
+    // int age;
+    // printf("enter your age :");
+    // scanf("%d", &age); //don't put \n after %d.
+    // if (age > 18 && age<35 )
+    // {
+    //     printf("You are an adult now \n");
+    // }
+    // else if (age < 0)
+    // {
+    //     printf("ulta paida hua tha??🤨🤨 \n");
+    // }
+    // else if (age > 35 && age < 70){
+    //     printf("Teri jawani ab nahi bachi bhai \n");
+    // }
+    // else
+    // {
+    //     printf("Bhai tum ab Budhe hogye ho. \n");
+    // }
+    
+
+    // age > 18 ? printf("adult \n"):printf("not an adult \n"); //Ternery operator
+
+
+    // int day;
+    // printf("enter day number (1-7) :");
+    // scanf("%d", &day); 
+    // switch (day){
+    //     // case (arg) -- arg can be char, int or float depending on the 
+    //     //type we have declared for variable.
+    //     case 1 : printf("MON \n");
+    //              break;
+    //     case 2 : printf("TUE \n");
+    //              break;
+    //     case 3 : printf("WED \n");
+    //              break;
+    //     case 4 : printf("THURS \n");
+    //              break;
+    //     case 5 : printf("FRI \n");
+    //              break;
+    //     case 6 : printf("SAT \n");
+    //              break;
+    //     case 7 : printf("SUN \n");
+    //              break;
+    //     default:
+    //     printf("Not a week's day!! \n");
+    // }
+
+    
+
+    // char character;
+    // printf("enter your character");
+    // scanf("%c", &character); 
+    // if (character >='a' && character <= 'z'){
+    //     printf("Your charcter is in lower case \n");
+    // }
+    // else if (character >= 'A' && character <= 'Z'){
+    //     printf("Your character is in upper case \n");
+    // }
+    // else{
+    //     printf("Your character is not an alphabet \n");
+    // }
+    //ascii value of a is 97
+    //character >= 'a' and character >= 97 is same.
+
+
+    // int age, marks;
+    // printf("Enter your age \n");
+    // scanf("%d", &age);
+    // printf("Enter your marks \n");
+    // scanf("%d", &marks);
+    // switch (age)
+    // {
+    // case 10: printf("your age is 10");
+    //         switch (marks)
+    //         {
+    //         case 100: printf("your marks are 100");
+    //             break;
+            
+    //         default: printf("your marks are not 10");
+    //             break;
+    //         }
+    //     break;
+    
+    // default:
+    //     break;
+    // }
+    
+    return 0;
+}
