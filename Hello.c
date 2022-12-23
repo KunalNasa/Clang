a7edd20856e592bc6bd9fb741cd07edaa290f085
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-12-23 19:53:56 +0530

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,21 @@
+#include<stdio.h> // pre processor directive.
+/*
+This is a multiline comment. 
+*/
+int main(){  
+    // int number = 25;
+    // char star = '*';
+    // age = 26;
+    // float pi = 3.14;  //under score or character in variable's name
+    // int total_amount = 100;
+    // printf("%d ", number);
+    int age = 24;
+    float pi = 3.14;
+    char hashtag = '#';
+    printf("age is %d \n" , age);
+    printf("for float value %f \n", pi);
+    printf("character is %c \n", hashtag);
+    
+    
+    return 0;
+}
