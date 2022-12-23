fe24986f7b7a30f7a0fe9c11f1c83e5022cd6be9
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-12-23 19:53:07 +0530

    Add files via upload

---
+++ formatspecifier.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define PI 3.14 // defined a constant.
+int main()
+{
+    int a = 10;
+    float b = 2.56;
+    // printf("The value of a is %d and the value of b is %f \n",a,b);
+    // %l - long
+    //%lf - double
+    //%LF - Long double  
+
+    // printf("%10.4f\n", b); //skip 5 spaces before b value.
+
+    // printf("%f \n", PI);
+    // printf("my backslash \\ %f \n", PI);
+    // printf("my tab \t\t and sound \a \n");
+    
+    return 0;
+}
