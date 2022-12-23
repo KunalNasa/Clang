e17fd329821520a9cee6d5c5c0e7330ca3296b1e
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-12-23 19:52:25 +0530

    Add files via upload

---
+++ forloop.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    // for (int i = 0; i <= 10; i++)
+    // {
+    //     printf("%d ", i);
+    // }
+    // printf("%lu \n", sizeof(float));
+
+    // int k;
+    // scanf("%d", &k);
+    // // printf("The table of ", k, "is\n");
+
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     printf("%d\n", i*k);
+    // }
+
+
+    // int i = 0, j =0;
+    // for(;i<10;i++)
+    // {
+    //     printf("%d %d\n", i, j);
+    // }
+
+
+    int i = 0, j =0;
+    for(;j<5, i<10;i++)
+    {
+        printf("%d %d\n", i, j);
+        j++;
+    }
+    return 0;
+}
