72a6d16d73d79e6582d4812ae7005643af7de20b
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2023-01-11 19:15:06 +0530

    Add files via upload

---
+++ dynamicmemoryallo.c
@@ -0,0 +1,16 @@
+//dynamic memory allocation - allocate memory in runtime.
+//or way in which size of a data structure can be changed during runtime.
+//memory assigned to a program in a typical architecture can be broken down into four segments:
+//1- code
+//2- static/global variables
+//(i) data segment - for initialized storage.
+//(ii) bss segment - for uninitialized storage.
+//3- stack
+//4- heap
+//C program:Stack Overflow - Typical example includes recursion with wrong base condition.
+#include<stdio.h>
+int main()
+{
+    
+    return 0;
+}
